2f509a619e05d1f90fe7e9e43be64c93f8ebc164
KhanhPham050499 <phamngocbaokhanh2015@gmail.com>
2020-09-09 03:43:51 +0700

    Updates

---
+++ precache-manifest.c3a45b9544c4823baef070b7d637386e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f7f83b1cf737133f19cb982d48d3e9",
+    "url": "/quanlytruong/index.html"
+  },
+  {
+    "revision": "2bbd6eeaca90112a9f98",
+    "url": "/quanlytruong/static/css/main.5292a108.chunk.css"
+  },
+  {
+    "revision": "49a738203353a3c66a89",
+    "url": "/quanlytruong/static/js/2.eb58acc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quanlytruong/static/js/2.eb58acc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bbd6eeaca90112a9f98",
+    "url": "/quanlytruong/static/js/main.6a76a186.chunk.js"
+  },
+  {
+    "revision": "2ccb9ed3fef2f063aa82",
+    "url": "/quanlytruong/static/js/runtime-main.2f6358ce.js"
+  }
+]);
